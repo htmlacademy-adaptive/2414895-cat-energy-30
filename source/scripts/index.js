64b471dcbeb83d719e367f18a6db95bd5c3b7170
Kruk-du-soleil <kruk.svetlana@gmail.com>
2024-01-23 18:15:56 +0300

    module7_part1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.main-nav');
+let menuLinks = menu.querySelectorAll('.main-nav__item-link');
+
+menu.classList.remove('main-nav--nojs');
+burger.classList.remove('burger--nojs');
+
+burger.addEventListener('click',
+function(){
+    burger.classList.toggle('burger--active');
+    menu.classList.toggle('main-nav--active');
+});
+
+menuLinks.forEach(function(el){
+    el.addEventListener('click', function() {
+        burger.classList.remove('burger--active');
+        menu.classList.remove('main-nav--active');
+    });
+});
