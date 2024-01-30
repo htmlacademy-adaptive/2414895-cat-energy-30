b5cf4b9edb4340af0eb9a477aee2f0d3fb0e9d8c
Kruk-du-soleil <kruk.svetlana@gmail.com>
2024-01-30 17:42:40 +0300

    module9-corr

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,9 +2,11 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.main-nav');
 const menuLinks = menu.querySelectorAll('.main-nav__item-link');
+const map = document.querySelector('.contacts__map-image');
 
 menu.classList.remove('main-nav--nojs');
 burger.classList.remove('burger--nojs');
+map.classList.remove('contacts__map-image--nojs');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger--active');
